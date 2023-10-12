95cfa9176ce6b4fcf15ad031c4c98a4e92308ac9
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-10-12 12:44:27 +0530

    path resolve configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      // Create aliases for your folders
+      // "@Common": path.resolve(__dirname, "src/components/Common"),
+      // "@Script": path.resolve(__dirname, "src/components/Script"),
+      // "@Pages": path.resolve(__dirname, "src/Pages"),
+      { find: "@Pages", replacement: path.resolve(__dirname, "src/Pages") },
+      {
+        find: "@Common",
+        replacement: path.resolve(__dirname, "src/components/Common"),
+      },
+      {
+        find: "@Script",
+        replacement: path.resolve(__dirname, "src/components/Script"),
+      },
+      {
+        find: "@Images",
+        replacement: path.resolve(__dirname, "src/assets/images"),
+      },
+    ],
+  },
+});
