615f23f909ba31b56de9f1c7d399097131c7c996
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-10-12 22:13:11 +0530

    eslint and alias configs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,16 +7,65 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+          ['@pages', './src/pages'],
+          ['@common', './src/components/common'],
+          ['@script', './src/components/script'],
+          ['@assets', './src/assets'],
+        ],
+      },
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'prettier/prettier': 'error'
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 0,
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always', // Add a newline between different import groups
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@pages/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@common/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@script/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@assets/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal'],
+      },
+    ],
   },
-}
+};
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,29 +1,26 @@
 /* eslint-disable no-undef */
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      // Create aliases for your folders
-      // "@Common": path.resolve(__dirname, "src/components/Common"),
-      // "@Script": path.resolve(__dirname, "src/components/Script"),
-      // "@Pages": path.resolve(__dirname, "src/Pages"),
-      { find: "@Pages", replacement: path.resolve(__dirname, "src/Pages") },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
       {
-        find: "@Common",
-        replacement: path.resolve(__dirname, "src/components/Common"),
+        find: '@common',
+        replacement: path.resolve(__dirname, 'src/components/common'),
       },
       {
-        find: "@Script",
-        replacement: path.resolve(__dirname, "src/components/Script"),
+        find: '@script',
+        replacement: path.resolve(__dirname, 'src/components/script'),
       },
       {
-        find: "@Images",
-        replacement: path.resolve(__dirname, "src/assets/images"),
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
       },
     ],
   },
