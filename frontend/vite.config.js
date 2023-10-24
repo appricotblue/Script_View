7e5af90ae4fc1adcfb765fe4fcb8a1b7d2b09b15
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-10-24 07:19:22 +0530

    base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: [
       { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
