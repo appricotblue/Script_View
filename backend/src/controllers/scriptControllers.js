d6f87ec21fc0107df0727f0269118707928fd224
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-11-07 10:25:24 +0530

    puppeteer config

--- backend/src/controllers/scriptControllers.js
+++ backend/src/controllers/scriptControllers.js
@@ -64,7 +64,10 @@ module.exports = {
 
     if (docFormatValidator.pdf) {
       try {
-        const browser = await puppeteer.launch({ headless: "new" });
+        const browser = await puppeteer.launch({
+          headless: "new",
+          args: ["--no-sandbox"],
+        });
         const page = await browser.newPage();
 
         await page.setContent(
